42684b57acf362d482a6f645f3e84cdba05a5510
Wiles <2190677233@qq.com>
2026-02-17 15:16:26 +0800

    # feat: run backward code

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from api.price_api import router as price_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="Quant API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # 开发阶段允许所有来源
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# 注册接口
+app.include_router(price_router)
