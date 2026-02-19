02582a0f96636c63e5bd2dc3533fa94605abfc5a
Wiles <2190677233@qq.com>
2026-02-19 21:33:01 +0800

    # chore: no code changed

--- api/price_api.py
+++ api/price_api.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 router = APIRouter(prefix="/api")
 
+# eg: http://http://127.0.0.1:8000/api/price?code=sh.600004
 @router.get("/price")
 def get_price(code: str):
     df = pd.read_csv(f"./data/price_data/{code}.csv")
     return df.to_dict(orient="records")
+
+
