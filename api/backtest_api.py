dc1a15a8b2310e4e0c1f3e2010aad8db2a6e4a77
Wiles <2190677233@qq.com>
2026-02-21 14:03:12 +0800

    # feat: add exit strategy arg

--- api/backtest_api.py
+++ api/backtest_api.py
@@ -5,6 +5,6 @@ router = APIRouter(prefix="/api")
 
 # eg: http://127.0.0.1:8000/api/backtest?code=sh.600004&strategy=ma5 diverge
 @router.get("/backtest")
-def get_backtest(code: str, strategy: str):
-    df = main_backtest(code, strategy, plot_equity_curve=False)
+def get_backtest(code: str, entry_strategy: str, exit_strategy: str):
+    df = main_backtest(code, entry_strategy, exit_strategy, plot_equity_curve=False)
     return df.to_dict(orient="records")
