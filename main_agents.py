dafabc7cce53f942fce03a39d231837f6310e4a5
Wiles <2190677233@qq.com>
2026-03-02 22:14:00 +0800

    # feat: add trade date smooth fuc

--- main_agents.py
+++ main_agents.py
@@ -1,8 +1,12 @@
+import pandas as pd
+
 from wiles_agents.graph.trading_graph import TradingAgentsGraph
 from wiles_agents.default_config import DEFAULT_CONFIG
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 import warnings
+import akshare as ak
 warnings.filterwarnings("ignore")
 
 # Load environment variables from .env file
@@ -27,8 +31,16 @@ config["data_vendors"] = {
 # Initialize with custom config
 ta = TradingAgentsGraph(debug=True, config=config)
 
+# 日期处理，若输入未来日期，则自动替换成近一个交易日
+code, trade_date = "sh.600004", "2026-06-15"
+trade_calendar = ak.tool_trade_date_hist_sina()
+trade_calendar["trade_date"] = pd.to_datetime(trade_calendar["trade_date"])
+today = datetime.today().strftime("%Y-%m-%d")
+calendar = trade_calendar[(trade_calendar["trade_date"]<=pd.to_datetime(trade_date)) & (trade_calendar["trade_date"]<=pd.to_datetime(today))]
+trade_date = str(calendar["trade_date"].iloc[-1])[:10]
+
 # forward propagate
-_, decision = ta.propagate("sh.600004", "2026-01-17")
+_, decision = ta.propagate(code, trade_date)
 print(decision)
 
 # Memorize mistakes and reflect
