93029e6f9d769addf9c15dce6a9dbf81ce3c6add
Wiles <2190677233@qq.com>
2026-03-07 15:43:43 +0800

    # refactor: add get social tools

--- wiles_agents/agents/analysts/social_media_analyst.py
+++ wiles_agents/agents/analysts/social_media_analyst.py
@@ -1,7 +1,7 @@
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 import time
 import json
-from wiles_agents.agents.utils.agent_utils import get_news
+from wiles_agents.agents.utils.agent_utils import get_social
 from wiles_agents.dataflows.config import get_config
 
 
@@ -12,12 +12,13 @@ def create_social_media_analyst(llm):
         company_name = state["company_of_interest"]
 
         tools = [
-            get_news,
+            get_social,
         ]
 
         system_message = (
-            "You are a social media and company specific news researcher/analyst tasked with analyzing social media posts, recent company news, and public sentiment for a specific company over the past week. You will be given a company's name your objective is to write a comprehensive long report detailing your analysis, insights, and implications for traders and investors on this company's current state after looking at social media and what people are saying about that company, analyzing sentiment data of what people feel each day about the company, and looking at recent company news. Use the get_news(query, start_date, end_date) tool to search for company-specific news and social media discussions. Try to look at all sources possible from social media to sentiment to news. Do not simply state the trends are mixed, provide detailed and finegrained analysis and insights that may help traders make decisions."
-            + """ Make sure to append a Markdown table at the end of the report to organize key points in the report, organized and easy to read.""",
+            "You are a social media and company specific news researcher/analyst tasked with analyzing social media posts, recent company news, and public sentiment for a specific company over the past week. You will be given a company's name your objective is to write a comprehensive long report detailing your analysis, insights, and implications for traders and investors on this company's current state after looking at social media and what people are saying about that company, analyzing sentiment data of what people feel each day about the company, and looking at recent company news. Use the get_social(ticker, curr_date, lookback_days) tool to search for company-specific news and social media discussions. Try to look at all sources possible from social media to sentiment to news. Do not simply state the trends are mixed, provide detailed and finegrained analysis and insights that may help traders make decisions."
+            + """ Make sure to append a Markdown table at the end of the report to organize key points in the report, organized and easy to read."""
+            + """If data is not supported for A share, break and return the not support message"""
         )
 
         prompt = ChatPromptTemplate.from_messages(
